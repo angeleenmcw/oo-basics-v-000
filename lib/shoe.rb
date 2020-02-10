8826524b061bdfc66f54b5d0ce817ff978e5b919
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-10 09:12:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class shoe 
+  
+  attr_accessor :size, :color, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+    BRAND = []
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def cobble
+    puts "Your shoe is as good as #{condition}"
+  end
+  
+end
+
